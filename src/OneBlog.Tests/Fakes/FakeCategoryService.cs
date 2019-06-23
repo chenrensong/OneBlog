34d83dec6a72b18e671c47192c79251e12f73ffb
chenrensong <chenrensong@outlook.com>
2019-06-23 23:02:04 +0800

    添加简单的单侧~

---
+++ src/OneBlog.Tests/Fakes/FakeCategoryService.cs
@@ -0,0 +1,58 @@
+﻿using OneBlog.Core.Services;
+using OneBlog.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace OneBlog.Tests.Fakes
+{
+    class FakeCategoryRepository : ICategoryService
+    {
+        Task<CategoryItem> ICategoryService.Add(CategoryItem item)
+        {
+            return Task.FromResult(item);
+        }
+
+        public Task<bool> Delete(string id)
+        {
+            return Task.FromResult(true);
+        }
+
+        public Task<IList<CategoryItem>> GetAll()
+        {
+            IList<CategoryItem> items = new List<CategoryItem>();
+            items.Add(new CategoryItem()
+            {
+                Id = Guid.NewGuid().ToString(),
+                Title = "test"
+            });
+            return Task.FromResult(items);
+        }
+
+        public Task<IList<CategoryItem>> Get(int take = 10, int skip = 0)
+        {
+            IList<CategoryItem> items = new List<CategoryItem>();
+            items.Add(new CategoryItem()
+            {
+                Id = Guid.NewGuid().ToString(),
+                Title = "test"
+            });
+            return Task.FromResult(items);
+        }
+
+        public Task<CategoryItem> Get(string id)
+        {
+            var item = new CategoryItem()
+            {
+                Id = id,
+                Title = "test"
+            };
+            return Task.FromResult(item);
+        }
+
+        Task<bool> ICategoryService.Update(CategoryItem item)
+        {
+            return Task.FromResult(true);
+        }
+    }
+}
---
+++ src/OneBlog.Tests/WebApi/CategoryControllerTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OneBlog.Areas.Admin.Controllers;
+using OneBlog.Tests.Fakes;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneBlog.Tests.WebApi
+{
+    [TestClass]
+    public class CategoryControllerTests
+    {
+        private CategoriesController _ctrl;
+        private HttpClient _httpClient;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _ctrl = new CategoriesController(new FakeCategoryRepository());
+
+            var server = new TestServer(WebHost.CreateDefaultBuilder()
+                .UseStartup<Startup>());
+            _httpClient = server.CreateClient();
+        }
+
+        [TestMethod]
+        public async Task CategoryControllerGet()
+        {
+            var results = await _ctrl.Get(0, 0);
+            Assert.IsTrue(results.Count >= 0);
+        }
+
+        [TestMethod]
+        public async Task CategoryControllerGetById()
+        {
+            var blog = await _ctrl.Get(Guid.NewGuid().ToString());
+            Assert.IsTrue(blog is OkObjectResult);
+        }
+
+    }
+}
